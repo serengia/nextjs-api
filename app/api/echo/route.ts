a5e3147d9eedb92b7bdafa09dabe1bf64ef9bc9b
James Serengia <jamesserengia@gmail.com>
2024-03-25 22:28:17 +0300

    Update echo route.ts

--- app/api/echo/route.ts
+++ app/api/echo/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Response) {
+export async function GET(request: Request) {
     const {searchParams} = new URL(request.url)
     const name = searchParams.get("name")
     const country = searchParams.get("country")
