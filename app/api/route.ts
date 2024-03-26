b050093607c965b53d121bf51accc2af17b97140
James Serengia <jamesserengia@gmail.com>
2024-03-26 12:29:50 +0300

    Add rate limiter

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,25 @@
 import { NextResponse } from "next/server";
+import { limiter } from "./routes-config/limiter";
 
-export async function GET(){
-    return NextResponse.json({message: "Welcome to api homepage"})
+export async function GET(request: Request) {
+  const remainingTokens = await limiter.removeTokens(1);
+  console.log("REMAINING TOKENS:: ", remainingTokens);
+
+  const origin = request.headers.get("origin");
+  if (remainingTokens < 0) {
+    return NextResponse.json(
+      {
+        message: "Too many requests",
+      },
+      {
+        status: 429,
+        statusText: "Too many requests.",
+        headers: {
+          "Content-Type": "text/plain",
+          "Access-Control-Allow-Origin": origin || "*",
+        },
+      }
+    );
+  }
+  return NextResponse.json({ message: "Welcome to api homepage" });
 }
